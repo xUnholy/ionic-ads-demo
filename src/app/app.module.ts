a6acd879216d8ff089903b2d17da8bbb83c75631
Michael <Michael.Fornaro@anz.com>
2019-05-17 12:00:22 +1000

    AdmobFree demo working with Test ads

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AdMobFree } from '@ionic-native/admob-free/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    AdMobFree,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform, AlertController } from '@ionic/angular';
+import { AdMobFree, AdMobFreeBannerConfig } from '@ionic-native/admob-free/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(
+    public platform: Platform,
+    public alertController: AlertController,
+    private admobFree: AdMobFree
+  ) {}
+
+  showBannerAdFreeTest() {
+    const bannerConfig: AdMobFreeBannerConfig = {
+      id: "ca-app-pub-9306767475974879/9489644613",
+      isTesting: true,
+      autoShow: true
+     };
+     this.admobFree.banner.config(bannerConfig);
+     
+     this.admobFree.banner.prepare()
+       .then(() => {
+         // banner Ad is ready
+         // if we set autoShow to false, then we will need to call the show method here
+       })
+       .catch(e => console.log(e)); 
+  }
+
+  showBannerAdFree() {
+    const bannerConfig: AdMobFreeBannerConfig = {
+      id: "ca-app-pub-9306767475974879/9489644613",
+      autoShow: true
+     };
+     this.admobFree.banner.config(bannerConfig);
+     
+     this.admobFree.banner.prepare()
+       .then(() => {
+         // banner Ad is ready
+         // if we set autoShow to false, then we will need to call the show method here
+       })
+       .catch(e => console.log(e)); 
+  }
 
 }
+
+
